fcc2f801eb7e521971e267638f2006c9328c09fe
Niklas Wünstel <niklas@Macbook-von-Niklas.local>
2018-12-14 15:41:06 +0100

    Erster Commit

---
+++ projectdiscussion.R
@@ -0,0 +1,25 @@
+projekte <- c()
+
+Thesetcoverproblem
+Schedulingjobswithdeadlines
+Schedulingjobsonparallelmachines
+Thetravelingsalesmanproblem
+Uncapacitatedfacilitylocationproblem
+Hidden Markov Models
+Image de-noising
+Clustering
+Kernel smoother
+Kerneldensityestimation
+Kernelregression
+LASSO, Ridge and friends
+Linear regression
+Logistic regression
+Network
+Neural networks and Stochastic Gradient Descent
+Kalman and Particle Filters
+Time series
+Gaussian Process Regression
+OSE
+LDA and SVM
+Sampling Methods
+Image Segmentation
