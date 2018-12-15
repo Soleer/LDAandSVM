85bdb24babf90b407e3c2e1426cf73874ccb21c8
Niklas Wünstel <niklas@dhcp650-210.laptop-wlc.uni-heidelberg.de>
2018-12-15 17:10:15 +0100

    Documentation

--- R/emp_cov.R
+++ R/emp_cov.R
@@ -1,3 +1,11 @@
+#' emp_cov
+#' 
+#' @param x A numeric Vector
+#' @param y A numeric Vector of same length as x
+#' @return The empirical Covariance of x and y
+#' @examples 
+#' emp_cov(c(1, 2, 3), c(1, 2, 3))
+#' emp_cov(c(1, 2, 3), c(3, 2, 1))
 emp_cov <- function(x, y) {
   Ex <- sum(x) / length(x)
   Ey <- sum(y) / length(y)
--- R/moore_penrose.R
+++ R/moore_penrose.R
@@ -1,5 +1,12 @@
 #(i)
-lambda_plus <- function(x) {
+#' lambda_plus
+#' 
+#' @param vec A Vector
+#' @return the Lambda-Plus vector of the given Vector
+#' @examples
+#' lambda_plus(c(1,4,2))
+#' lambda_plus(c(1, 0, 4, 0))
+lambda_plus <- function(vec) {
   sapply(x, function(y) {
     if (all.equal(y, 0) != TRUE)
       return(1 / y)
@@ -8,6 +15,13 @@ lambda_plus <- function(x) {
 }
 
 #(ii)
+#' moore_penrose
+#'
+#' @param mat A symmetric, positiv semidefinte Matrix
+#' @return The Moore-Penrose Inverse of the given Matrix
+#' @example
+#' A <- matrix(c(1, 0, 0, 2), ncol = 2)
+#' moore_penrose(A)
 moore_penrose <- function(mat) {
   res <- eigen(mat, symmetric = TRUE)
   diagplus <- diag(lambda_plus(res$values))
