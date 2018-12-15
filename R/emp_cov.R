0098bf0ea692f9a6fc73f49561af6ccdcbfafe90
Niklas Wünstel <niklas@dhcp650-210.laptop-wlc.uni-heidelberg.de>
2018-12-15 18:19:16 +0100

    Problem 6

--- R/emp_cov.R
+++ R/emp_cov.R
@@ -9,5 +9,5 @@
 emp_cov <- function(x, y) {
   Ex <- sum(x) / length(x)
   Ey <- sum(y) / length(y)
-  return(((x - Ex) %*% (y - Ey)) / length(x))
+  return( ( (x - Ex) %*% (y - Ey)) / length(x))
 }
