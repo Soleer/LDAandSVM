1f5ff788174b871429ce78c31521ec148a7e5850
Niklas Wünstel <niklas@dhcp650-210.laptop-wlc.uni-heidelberg.de>
2018-12-15 18:00:07 +0100

    Tests

--- tests/testthat/test-emp_cov.R
+++ tests/testthat/test-emp_cov.R
@@ -1,5 +1,15 @@
 context("test-emp_cov")
-
+test <-
+  c(
+    c(1, 2, 3, 4),
+    c(1, 2, 3, 4),
+    c(1, 2, 3, 4),
+    1:100,
+    c(1, 2, 3, 4),
+    c(4, 3, 2, 1),
+    c(4, 2, 1, 1),
+    rep(0, 100)
+  )
 test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+  lapply(test, function(x) expect_equal(emp_cov(x), emp_cov(x)))
 })
--- tests/testthat/test-moore_penrose.R
+++ tests/testthat/test-moore_penrose.R
@@ -1,5 +1,9 @@
 context("test-moore_penrose")
-
+A <- matrix(c(1,2,2,4))
+Aplus <- moore_penrose(A)
 test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+  expect_equal(A%*%Aplus%*%A, A)
+  expect_equal(Aplus%*%A%*%Aplus,Aplus)
+  expect_true(isSymmetric(A%*%Aplus))
+  expect_true(isSymmetric(Aplus%*%A))
 })
