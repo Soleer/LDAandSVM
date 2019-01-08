66a81890e05acb59b3b9dd9a448c2e897ef3f247
Niklas Wünstel <niklas@Macbook-von-Niklas.local>
2019-01-08 20:49:28 +0100

    improved test

--- R/chapter4_3.R
+++ R/chapter4_3.R
@@ -1,19 +1,20 @@
 library(ggplot2)
 set.seed(0)
-
+rep('A', 10)
 #Chapter 4.3
-
 # Gauß test data n classes same sigma:
 make_test <- function(ninputs = 100,
                       nclasses = 2,
-                      sigma = 0.6) {
-  xcoord <- sample(-4:4, nclasses, replace = TRUE)
-  ycoord <- sample(-4:4, nclasses, replace = TRUE)
+                      sigma = 0.6,
+                      x = c(-5, 5),
+                      y = c(-5, 5)) {
+  xcoord <- sample(x[1]:x[2], nclasses, replace = TRUE)
+  ycoord <- sample(y[1]:y[2], nclasses, replace = TRUE)
   test <-
     data.frame(
       x = rnorm(ninputs, xcoord[1], sigma),
       y = rnorm(ninputs, ycoord[1], sigma),
-      class = rep('A', ninputs)
+      class = rep('A', times = ninputs)
     )
   sapply(2:nclasses, function(i) {
     class <- LETTERS[i]
@@ -21,7 +22,7 @@ make_test <- function(ninputs = 100,
       rbind(test, data.frame(
         x = rnorm(ninputs, xcoord[i], sigma),
         y = rnorm(ninputs, ycoord[i], sigma),
-        class = rep(class, ninputs)
+        class = rep(class, times = ninputs)
       ))
   })
   return(test)
@@ -110,7 +111,7 @@ get_Y_Value <- function(z, ploty) {
 getseperatorfun <- function(a,
                             b,
                             rf,
-                            ploty = c(-5, 5),
+                            y = c(-5, 5),
                             inv = FALSE) {
   getzero <- create_id(a, b, rf)
   sep <- function(x) {
@@ -124,7 +125,7 @@ getseperatorfun <- function(a,
       }
       
     }
-    y <- get_Y_Value(z, ploty)
+    y <- get_Y_Value(z, y)
     if (inverse == FALSE) {
       if (is.na(y) || which.max(rf(c(x, y))) == a ||
           which.max(rf(c(x, y))) == b) {
@@ -144,24 +145,24 @@ getseperatorfun <- function(a,
 }
 
 make_seperator <- function(classf,
-                          nclass,
-                          min = -5,
-                          max = 5,
-                          acc = 100) {
-  sepdata <- data.frame(xval = seq(min, max, length.out = acc))
+                           nclass,
+                           x = c(-5, 5),
+                           y = c(-5, 5),
+                           nx = 100) {
+  sepdata <- data.frame(xval = seq(x[1], x[2], length.out = nx))
   iT <- integer(0)
   k <- 1
   sapply(1:(nclass - 1), function(i) {
     sapply((i + 1):nclass, function(j) {
       k <<- k + 1
-      s <- getseperatorfun(i, j, classf, ploty = c(min, max))
+      s <- getseperatorfun(i, j, classf, y = y)
       sepdata[k] <<- sapply(sepdata$xval, s)
       iT[k] <<- FALSE
       if (length(sepdata[!is.na(sepdata[k]), k]) <= 1) {
         s <- getseperatorfun(i,
                              j,
                              classf,
-                             ploty = c(min, max),
+                             y = x,
                              inv = TRUE)
         sepdata[k] <<- sapply(sepdata$xval, s)
         iT[k] <<- TRUE
@@ -171,11 +172,17 @@ make_seperator <- function(classf,
   return(list(sepdata, iT))
 }
 
-make_plot <- function(data, results) {
+make_plot <- function(data,
+                      results,
+                      y = c(-5, 5),
+                      x = c(-5, 5),
+                      ppu = 10) {
   #prama
   f <- LDA(data, results)
   classfun <- LDAclass(data, results)
   n <- length(unique(results))
+  xtimes <- (x[2] - x[1]) * ppu
+  ytimes <- (y[2] - y[1]) * ppu
   #
   mainplotdata <- cbind(data, results)
   mainplot <- ggplot()
@@ -189,41 +196,46 @@ make_plot <- function(data, results) {
     )
   #background
   background <-
-    data.frame(x = rep(seq(-5, 5, length.out = 50), time = 50), y = c(sapply(seq(-5, 5, length.out = 50), function(x)
-      rep(x, times = 50))))
-  background$class <- apply(background, 1, classfun)
-  mainplot <- mainplot + geom_jitter(
-      data = background,
-      aes(x, y, color = class),
-      shape = 3,
-      height = 0 ,
-      width = 0
-    )
-  #lines
-  seps <- make_seperator(f, n)
-  sepdata <- seps[[1]]
-  iT <- seps[[2]]
-  #addlines
-  
-  sapply(1:choose(n, 2) + 1, function(i) {
-    if (any(!is.na(sepdata[paste0('V', i)]))) {
-      if (!iT[i]) {
-        mainplot <<-
-          mainplot + geom_path(data = sepdata,
-                               aes_string(x = 'xval', y = paste0('V', i)),
-                               na.rm = TRUE)
-      }
-      else{
-        mainplot <<-
-          mainplot + geom_path(data = sepdata,
-                               aes_string(x = paste0('V', i), y = 'xval'),
-                               na.rm = TRUE)
-      }
-    }
-  })
-  return(mainplot)
+    data.frame(
+      x = rep(seq(x[1], x[2], length.out = xtimes), times = ytimes),
+      y = c(sapply(
+        seq(y[1], y[2], length.out = ytimes), function(x)
+          rep(x, times = xtimes))
+      ))
+      background$class <- apply(background, 1, classfun)
+      mainplot <- mainplot + geom_jitter(
+        data = background,
+        aes(x, y, color = class),
+        shape = 3,
+        height = 0 ,
+        width = 0
+      )
+      #lines
+      seps <- make_seperator(f, n, x, y, xtimes)
+      sepdata <- seps[[1]]
+      iT <- seps[[2]]
+      #addlines
+      
+      sapply(1:choose(n, 2) + 1, function(i) {
+        if (any(!is.na(sepdata[paste0('V', i)]))) {
+          if (!iT[i]) {
+            mainplot <<-
+              mainplot + geom_path(data = sepdata,
+                                   aes_string(x = 'xval', y = paste0('V', i)),
+                                   na.rm = TRUE)
+          }
+          else{
+            mainplot <<-
+              mainplot + geom_path(data = sepdata,
+                                   aes_string(x = paste0('V', i), y = 'xval'),
+                                   na.rm = TRUE)
+          }
+        }
+      })
+      return(mainplot)
 }
-test <- make_test(nclasses = 7,sigma = 0.6)
-testplot <- make_plot(test[c('x', 'y')], test$class)
+x <- c(-10, 10)
+y <- c(-10, 10)
+test <- make_test(100, nclasses = 10, sigma =2 , x, y)
+testplot <- make_plot(test[c('x', 'y')], test$class, x, y,ppu = 5)
 testplot
-
