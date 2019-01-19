4e8e033a5f137109f75e5fd0719a0c5d2fc504ac
Niklas Wünstel <niklas@Macbook-von-Niklas.local>
2019-01-19 15:38:15 +0100

    calcerror

--- R/Main.R
+++ R/Main.R
@@ -10,25 +10,28 @@ source("R/plot_functions.R")
 set.seed(0)
 
 ##Analyse
-sig <- c(1,1.5,2,2.5,1.3,1.1,2.1,1.8)
+sig <- c(1.5,2,2.5,1.3,1.1,2.1,1.8)
+dimension <- 2
+
 test <- make_test(100,
-                  nparam = 4,
-                  nclasses = 8,
+                  nparam = dimension,
+                  nclasses = 2,
                   sigma = sig)
 
 
 ### PDA
-f <- classify(unique(test$class), PDA(test[1:4], test$class, base = "quad"))
-liste <- plot_error(test[1:4], test$class, f)
+f <- classify(unique(test$class), PDA(test[1:dimension], test$class, base = "quad"))
+liste <- plot_error(test[1:dimension], test$class, f)
 p1 <- do.call(grid.arrange, liste)
 testplot <-
-  make_2D_plot(test[1:4],
+  make_2D_plot(test[1:dimension],
                test$class,
                f,
                ppu = 5)
 plotlist <- list(p1, testplot)
 nice <- do.call("grid.arrange", c(plotlist, ncol = 2, top = "PDA"))
 
+calc_error(test[1:dimension],test$class,f2)
 ggsave('PDA.png',
        plot = nice,
        device = 'png',
@@ -36,11 +39,11 @@ ggsave('PDA.png',
 
 
 ### LDA
-f2 <- classify(unique(test$class), LDA(test[1:4], test$class))
-liste2 <- plot_error(test[1:4], test$class, f2)
+f2 <- classify(unique(test$class), LDA(test[1:dimension], test$class))
+liste2 <- plot_error(test[1:dimension], test$class, f2)
 p2 <- do.call(grid.arrange, liste2)
 testplot2 <-
-  make_2D_plot(test[1:4],
+  make_2D_plot(test[1:dimension],
                test$class,
                f2,
                ppu = 5)
@@ -55,11 +58,11 @@ ggsave('LDA.png',
 
 
 ### QDA
-f3 <- classify(unique(test$class), QDA(test[1:4], test$class))
-liste3 <- plot_error(test[1:4], test$class, f3)
+f3 <- classify(unique(test$class), QDA(test[1:dimension], test$class))
+liste3 <- plot_error(test[1:dimension], test$class, f3)
 p3 <- do.call(grid.arrange, liste3)
 testplot3 <-
-  make_2D_plot(test[1:4],
+  make_2D_plot(test[1:dimension],
                test$class,
                f3,
                ppu = 5)
