ff829a6f95bace0cb51a47cdab1e25e8ab9d7979
em178 <h.stein@stud.uni-heidelberg.de>
2019-02-17 11:57:06 +0100

    Updated Classifier_funs.R

--- R/Classifier_funs.R
+++ R/Classifier_funs.R
@@ -66,21 +66,21 @@ QDA <- function(data, results) {
 }
 
 
-PDA <- function(data, results, base) {
-  h <- basis_exp(base)
-  data_exp <- h(data)
-  G <- unique(results)
-  K <- length(G)
-  p <- log(pi_est(results))
-  mu <- mu_est(data, results)
-  sigma_list <- lapply(1:K, function(k) {
+PDA <- function(data, results, base) {            ##The PDA classification function. A function factory
+  h <- basis_exp(base)                            ##Gets the basis expansion function
+  data_exp <- h(data)                             ##Expands the data via the expansion function
+  G <- unique(results)                            ##Vector containing all unique classes
+  K <- length(G)                                  ##Number of unique classes
+  p <- log(pi_est(results))                       ##Probability of one class occuring
+  mu <- mu_est(data, results)                     ##Vector of class centroid for each class
+  sigma_list <- lapply(1:K, function(k) {         ##Calculating the class specific covariance matrices of the expanded data and writing them to a list
     sigma_class(data_exp[results==G[k],],mu[k])
   })
-  Matrix <- lapply(sigma_list, function(x) solve(x + diag(0, nrow=nrow(x), ncol=ncol(x))))
+  Matrix <- lapply(sigma_list, function(x) solve(x + diag(0, nrow=nrow(x), ncol=ncol(x))))  ##Adding the Omega matrix (penalizer) to every class covariance matrix and getting the inverse 
   
-  delta <- function(x) {
+  delta <- function(x) {                          ##The distance function. The same as QDA but with a penalized distance function and with the expanded data.
     result <- sapply(1:K, function(k) {
-      -1 / 2 * log(det(Matrix[[k]])) - 1 / 2 * t(as.vector(h(x) - h(mu[k, ]))) %*% Matrix[[k]] %*% as.vector((h(x) - h(mu[k, ])))
+      -1 / 2 * log(det(Matrix[[k]])) - 1 / 2 * t(h(x) - h(mu[k, ])) %*% Matrix[[k]] %*% (h(x) - h(mu[k, ]))
     }) + p
     return(result)
   }
