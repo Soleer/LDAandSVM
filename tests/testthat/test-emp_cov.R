9be6a792cedea2dba52a17cda0dfdabf31f55ff7
Benedikt <benediktlauer@gmx.de>
2019-02-20 19:50:13 +0100

    testthat wurde verändert

--- tests/testthat/test-emp_cov.R
+++ tests/testthat/test-emp_cov.R
@@ -1,16 +1,16 @@
-context("test-emp_cov")
-test_that("emp_cov", {
-  test <-
-    c(
-      c(1, 2, 3, 4),
-      c(1, 2, 3, 4),
-      c(1, 2, 3, 4),
-      1:100,
-      c(1, 2, 3, 4),
-      c(4, 3, 2, 1),
-      c(4, 2, 1, 1),
-      rep(0, 100)
-    )
-  lapply(test, function(x)
-    expect_equal(emp_cov(x), emp_cov(x)))
-})
+# context("test-emp_cov")
+# test_that("emp_cov", {
+#   test <-
+#     c(
+#       c(1, 2, 3, 4),
+#       c(1, 2, 3, 4),
+#       c(1, 2, 3, 4),
+#       1:100,
+#       c(1, 2, 3, 4),
+#       c(4, 3, 2, 1),
+#       c(4, 2, 1, 1),
+#       rep(0, 100)
+#     )
+#   lapply(test, function(x)
+#     expect_equal(emp_cov(x), emp_cov(x)))
+# })
