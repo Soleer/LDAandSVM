8c0c909e81bddd3176934ba6d5e5280e8c3ac83a
Niklas Wünstel <niklas@dhcp663-127.laptop-wlc.uni-heidelberg.de>
2019-02-20 18:51:24 +0100

    move errorfunctions

--- R/Calc_error.R
+++ R/Calc_error.R
@@ -1,21 +1,28 @@
+calc_miss <- function(data, results, f){
+  estimations <- apply(data,1,f)
+  n <- length(results)
+  sum(results!=estimations)/n
+}
+
 
 #'calc_error
 #'
-#'Calculates the following propabilitys for a function of a data_set:
-#'[1]
-#'prop. of: class 'A' to be classified to a class 'B'
-#'prop. of: class 'A' to be classified to 'A'
-#'prop. of: class 'A' to be not classified to 'A' 
-#'[2]
-#'prop. of: classified to class 'A' and actually was class 'B'
-#'prop. of: classified to class 'A' and also was 'A'
-#'prop. of: classified to class 'A' but was not 'A'
-#'[3]
-#'prop. of: missclassification 
+#'Calculates the following propabilitys for a classificationfunction of a data_set:
+#'[[1]]                                                        #accessed over: 
+#'prop. of: class 'A' to be classified to a class 'B'          # ['B','A']
+#'prop. of: class 'A' to be classified to 'A'                  # ['right','A']
+#'prop. of: class 'A' to be not classified to 'A'              # ['wrong','A']
+#'[[2]]
+#'prop. of: classified to class 'A' and actually was class 'B' #['B','A']
+#'prop. of: classified to class 'A' and also was 'A'           #['right','A']
+#'prop. of: classified to class 'A' but was not 'A'            #['wrong','A']
+#'[[3]]
+#'prop. of: missclassification                                 #['miss']
 #'
 #'@param set A object of class 'data_set'
 #'@param name the functionname of the function in set
-#'@return A list with 3 entrys: 
+#'@return A list with 3 entrys. Entry [[1]] and [[2]] are matrixes and contain the values as described in the description.
+#' [[3]] is the propability of missclassification.
 calc_error <- function(set, name) {
   if (!is.data_set(set)) {
     stop("Input must be of class 'data_set' (?make_set)", call. = FALSE)
@@ -75,96 +82,3 @@ calc_error <- function(set, name) {
   miss <- round(miss, 2)
   return(list(probs_of_Data,probs_of_Results,miss=miss))
 }
-
-calc_totalMiss <- function(set, name){
-  #TODO nicht ganzen Funktionsaufruf von calc_error, eher andersherum
-  errors<-calc_error(set, name)
-  return(errors$miss)
-}
-
-calc_miss <- function(data, results, f){
-  estimations <- apply(data,1,f)
-  n <- length(results)
-  sum(results!=estimations)/n
-}
-#' 
-#' 
-#' 
-#' #'calc_error
-#' #'
-#' #'calculates the total error rate of of a classifaction function on a dataset
-#' #'
-#' #'@param data Dataframe of Parameters for all Observations
-#' #'@param results correct classes
-#' #'@param f classification function
-#' #'@return total error rate
-#' calc_error <- function(data, results, f) {
-#'   G <- unique(results)
-#'   
-#'   force(f)
-#'   y <- f(1:2)
-#'   print(y)
-#'   estimated <- apply(data, 1, f)
-#'   
-#'   of_Data <- lapply(G, function(class) {
-#'     c <- as.character(class)
-#'     t <- table(estimated[results == class])
-#'     number <- sum(t)
-#'     
-#'     order <- t[G]
-#'     order[is.na(order)] <- 0
-#'     classresults <- as.list(order / number)
-#'     
-#'     right <- t[c] / number
-#'     wrong <- (1 - right)
-#'     
-#'     return(col)
-#'   })
-#'   
-#'   probs_of_Data <-
-#'     data.frame(class = c(as.character(G), 'right', 'wrong'), of_Data)
-#'   
-#'   miss <-
-#'     sum(probs_of_Data[probs_of_Data$class == 'wrong', 1:length(G) + 1]) / length(G)
-#'   return(miss)
-#' }
-=======
-
-
-#'calc_error
-#'
-#'calculates the total error rate of of a classifaction function on a dataset
-#'
-#'@param data Dataframe of Parameters for all Observations
-#'@param results correct classes
-#'@param f classification function
-#'@return total error rate
-calc_totalMiss_ondata <- function(data, results, f) { #TODO 
-  G <- unique(results)
-
-  force(f)
-  estimated <- apply(data, 1, f)
-
-  of_Data <- lapply(G, function(class) {
-    c <- as.character(class)
-    t <- table(estimated[results == class])
-    number <- sum(t)
-
-    order <- t[G]
-    order[is.na(order)] <- 0
-    classresults <- as.list(order / number)
-
-    right <- t[c] / number
-    wrong <- (1 - right)
-
-    return(col)
-  })
-
-  probs_of_Data <-
-    data.frame(class = c(as.character(G), 'right', 'wrong'), of_Data)
-
-  miss <-
-    sum(probs_of_Data[probs_of_Data$class == 'wrong', 1:length(G) + 1]) / length(G)
-  return(miss)
-}
->>>>>>> b40f85e5f05bcf79d56811c3ed396f6b5e861220
